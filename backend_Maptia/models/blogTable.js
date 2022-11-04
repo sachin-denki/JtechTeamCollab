99f51128ca88657e02af5dc11295ad63edba07f7
afreen <afreen.banu@jtechworld.com>
2022-11-04 17:20:01 +0530

    master

--- backend_Maptia/models/blogTable.js
+++ backend_Maptia/models/blogTable.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose')
 
-const postSchema = mongoose.Schema({
-       // title:String
+const blogSchema = mongoose.Schema({
+
        userid: { type: Number, required: true },
        blogName: { type: String, required: true },
-       blockTitele: { type: String, required: true },
+       blockTitle: { type: String, required: true },
        blogContent: { type: String, required: true },
        blogImage: { type: String, required: true },
        category: { type: String, required: true },
-       blogedContry: { type: String, required: true }
+       Country: { type: String, required: true },
+       days: { type: String, required: true },
+       readTime: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('blogTable', postSchema)
+module.exports = mongoose.model('blogTable', blogSchema)
